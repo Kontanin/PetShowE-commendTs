b9bcb5c1d3e601951dfb96b496adbd02eca1155a
Kontanin <boss12853@gmail.com>
2024-06-05 19:43:10 +0700

    before pull

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -1,25 +1,33 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { Listbox, ListboxItem } from '@nextui-org/react';
+import { useUserStore } from '@/store/zustand';
 
 function MyProfile() {
-  const [selectedKeys, setSelectedKeys] = React.useState(new Set(['text']));
-  const selectedValue = React.useMemo(
-    () => Array.from(selectedKeys).join(', '),
-    [selectedKeys],
-  );
+  const router = useRouter();
+  const { isAuthenticated, checkToken } = useUserStore();
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      await checkToken();
+      if (!isAuthenticated) {
+        router.push('/'); // Redirect to home if not authenticated
+      }
+    };
+
+    checkAuthentication();
+  }, [isAuthenticated, checkToken, router]);
 
   return (
     <div className="p-6 bg-gray-100 text-center">
       <h6>My Profile</h6>
-
       <img
         className="w-64 h-64 overflow-hidden mx-auto mb-4 rounded-full shadow-lg"
         src="/mypic.jpg"
+        alt="Profile"
       />
-
       <h3 className="text-xl font-semibold mb-2">Josh do</h3>
-
       <div className="max-w-40 border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
         <Listbox
           aria-label="Actions"
@@ -30,7 +38,7 @@ function MyProfile() {
             <h3 className="text-xl text-left py-2">Information</h3>
           </ListboxItem>
           <ListboxItem key="copy">
-            <h3 className="text-xl text-left py-2">My Adress</h3>
+            <h3 className="text-xl text-left py-2">My Address</h3>
           </ListboxItem>
           <ListboxItem key="edit">
             <h3 className="text-xl text-left py-2">My Order</h3>

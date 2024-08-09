f69b60c96135a6df4a6900827797cdd8d67b36c9
Kontanin <boss12853@gmail.com>
2024-08-09 16:43:15 +0700

    delete prod

--- src/components/productslice/PriceTag.tsx
+++
@@ -1,9 +0,0 @@
-interface PriceTagProps {
-  price: number;
-  className?: string;
-}
-// sdfadsf
-
-export default function PriceTag({ price, className }: PriceTagProps) {
-  return <span className={`badge ${className}`}>{price}</span>;
-}
